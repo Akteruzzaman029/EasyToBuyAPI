974bcb78043cdadc6fd05e09206d28d0b4a85b47
AkterruzzamanEPS <md.akterruzzaman@eps.com.bd>
2025-07-27 01:41:33 +0600

    updated card issue

--- Core/ModelDto/Cart/CartResponseDto.cs
+++ Core/ModelDto/Cart/CartResponseDto.cs
@@ -4,6 +4,12 @@ namespace Core.ModelDto.Cart
 {
     public class CartResponseDto : CartModel
     {
+        public string ProductName { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Discount { get; set; }
+        public decimal UnitPriceAfterDiscount { get; set; }
+        public decimal TotalPriceAfterDiscount { get; set; }
 
     }
 }
