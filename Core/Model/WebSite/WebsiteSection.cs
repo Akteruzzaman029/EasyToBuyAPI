3ebd677567ac602dc7ec0968d397a2a74fa13025
Akteruzzaman-codezzi <akteruzzaman.codezzi@gmail.com>
2026-02-21 05:13:19 +0600

    update websection

---
+++ Core/Model/WebSite/WebsiteSection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model.WebSite;
+
+
+public class WebsiteSection : BaseEntity
+{
+    public string? Name { get; set; }
+    public string? HeaderName { get; set; }
+    public int? SequenceNo { get; set; }
+    public int? FileId { get; set; }
+}
